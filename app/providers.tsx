bca22024e701beed0968312a56e19ff1d9bbdc80
thinkdj <think.dj@gmail.com>
2023-03-05 20:27:25 +0530

    providers: sessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import React, { ReactNode } from 'react';
+import { SessionProvider } from "next-auth/react"
 
 interface ProvidersProps {
 	children: ReactNode;
 }
 import { MantineProvider } from '@mantine/core';
 
-export function Providers({ children }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps, session) {
 	return (
+		<SessionProvider session={session}>
 		<MantineProvider withGlobalStyles withNormalizeCSS>
 			{children}
 		</MantineProvider>
+		</SessionProvider>
 	);
 }
